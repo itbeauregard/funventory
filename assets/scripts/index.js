8be7c69cb3651421c6960fc14207f92b9a76e385
itbeauregard <i.t.beauregard@gmail.com>
2017-09-16 18:30:37 -0400

    Add jquery click handler to get all products button

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,6 +3,7 @@
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 const accountEvents = require('./database/events.js')
+const productEvents = require('./products/events.js')
 
 $(() => {
   setAPIOrigin(location, config)
@@ -19,4 +20,6 @@ $(() => {
   $('#signin-account').on('submit', accountEvents.onSignIn)
   $('#change-password').on('submit', accountEvents.onChangePassword)
   $('#signout-account').on('submit', accountEvents.onSignOut)
+
+  $('#get-table').on('click', productEvents.onGetAllProducts)
 })
