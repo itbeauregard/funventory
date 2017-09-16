9a3b2e045df79bb274a94b00e9b67e7ab7db0dda
itbeauregard <i.t.beauregard@gmail.com>
2017-09-16 18:33:31 -0400

    Add handlerbars compiler to ui.js
    onGetAllProductsSuccess function should draw the table of products

--- assets/scripts/products/ui.js
+++ assets/scripts/products/ui.js
@@ -18,9 +18,34 @@ const onUpdateProductFail = function () {
   console.log('Your edit fails')
 }
 
+const onGetAllProductsSuccess = function (data) {
+  console.log('first line inside function')
+  console.log(data.products)
+  $(() => {
+    const theTemplateScript = $('#product-inventory-template').html()
+    // Compile the template
+    const theTemplate = Handlebars.compile(theTemplateScript)
+    // Define our data object
+    const context = {
+      fields: data.products
+    }
+    // Pass our data to the template
+    const theCompiledHtml = theTemplate(context)
+    // Add the compiled html to the page
+    $('#table-holder').prepend(theCompiledHtml)
+    console.log('last line within function')
+  })
+}
+
+const onGetAllProductsError = function (response) {
+  console.error(response)
+}
+
 module.exports = {
   onCreateProductSuccess,
   onCreateProductFail,
   onUpdateProductSuccess,
-  onUpdateProductFail
+  onUpdateProductFail,
+  onGetAllProductsSuccess,
+  onGetAllProductsError
 }
